a119eb8a287496ca39a79fbd7a779c45fc3e07e1
tnjordan <spammy.whale@gmail.com>
2023-01-30 12:30:18 -0800

    q5 completed

--- hw1/Q5/wrangling_scripts/Q5.py
+++ hw1/Q5/wrangling_scripts/Q5.py
@@ -7,7 +7,7 @@ that will be supplied to the template for generating the table. """
 import csv
 
 def username():
-    return 'gburdell3'
+    return 'tjordan60'
 
 def data_wrangling():
     with open('data/movies.csv', 'r', encoding='utf-8') as f:
@@ -21,14 +21,19 @@ def data_wrangling():
             break
         
         # Read in each row
+        c = 0
         for row in reader:
+            if c == 100:
+                break
+            c += 1
             table.append(row)
-            
             # Only read first 100 data rows - [2 points] Q5.a
             ...
+
         
         # Order table by the last column - [3 points] Q5.b
         ...
+        s_table = sorted(table,key= lambda x: float(x[2]), reverse=True)
     
-    return header, table
+    return header, s_table
 
